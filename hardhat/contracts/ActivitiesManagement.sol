a01eecb058a5a60153caf704bb749e7de322dd37
Chris <61536264+10000-C@users.noreply.github.com>
2025-08-27 19:59:45 +0800

    feat(contracts): 添加活动管理功能
    - 新增 ActivitiesManagement 合约，实现活动创建、删除、查询和参与功能
    - 引入 AuthorityManagement 合约进行权限管理
    - 支持活动创建者删除活动
    - 支持用户参与活动，有限制参与人数和防止重复参与

---
+++ hardhat/contracts/ActivitiesManagement.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "./AuthorityManagement.sol";
+
+contract ActivitiesManagement { 
+    event ActivityCreated(uint256 indexed activityId, string dataCID);
+    struct Activity {
+        string dataCID; //IPFS CID for JSON metadata
+        address creator;
+        uint256 maxParticipants;
+        uint256 cuParticipants;
+        uint256 acticityId;
+    }
+    mapping(uint256 => mapping(address => bool)) public isParticipant;
+    AuthorityManagement private authorityManagement;
+    
+    mapping(uint256 => Activity) public activities;
+    uint256 public activityCount; 
+    
+    constructor(address _authorityManagementAddress) {
+        authorityManagement = AuthorityManagement(_authorityManagementAddress);
+    }
+
+    function createActivity(
+        string memory _dataCID,
+        uint256 _maxParticipants,
+        uint256 _cuParticipants
+    ) public onlyOrganizer {
+        activityCount++;
+        Activity memory newActivity = Activity({
+            dataCID: _dataCID,
+            creator: msg.sender,
+            maxParticipants: _maxParticipants,
+            cuParticipants: _cuParticipants,
+            acticityId: activityCount
+        });
+        activities[activityCount] = newActivity;
+        emit ActivityCreated(activityCount, _dataCID);
+    }
+
+    function deleteActivity(uint256 _activityId) public onlyOrganizer {
+        require(_activityId > 0 && _activityId <= activityCount, "Invalid activity ID");
+        require(activities[_activityId].creator == msg.sender, "Not the creator of the activity");
+        delete activities[_activityId];
+    } 
+
+    function getActivity(uint256 _activityId) public view returns (Activity memory) {
+        require(_activityId > 0 && _activityId <= activityCount, "Invalid activity ID");
+        return activities[_activityId];
+    }
+
+    function participateInActivity(uint256 _activityId) public onlyLegalUser {
+        require(_activityId > 0 && _activityId <= activityCount, "Invalid activity ID");
+        require(isParticipant[_activityId][msg.sender] == false, "Already participated in this activity");
+        require(activities[_activityId].cuParticipants<activities[_activityId].maxParticipants, "Activity is full");
+        
+        isParticipant[_activityId][msg.sender] = true;
+        activities[_activityId].cuParticipants++;
+    } 
+
+    modifier onlyOrganizer() {
+        require(authorityManagement.isAnOrganizer(msg.sender), "Not an organizer");
+        _;
+    }
+
+    modifier onlyLegalUser() {
+        require(!authorityManagement.isBlockedUser(msg.sender), "User is blocked");
+        _;
+    }
+}
