c9bc2a9b01d74a3392458dec4e5a00da44d7d9fb
Chris <61536264+10000-C@users.noreply.github.com>
2025-09-03 11:34:03 +0800

    feat(hardhat): 新增演示管理(DemosManagement)合约
    - 实现了演示提交和更新功能
    - 集成了活动管理和判决管理合约
    - 添加了参与者身份验证和演示ID验证

---
+++ hardhat/contracts/DemosManagement.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "./JudgementManagement.sol";
+import "./ActivitiesManagement.sol";
+
+contract DemosManagement { 
+
+    event DemoSubmitted(uint256 indexed activityId, Demo demo);
+    event DemoUpdated(uint256 indexed activityId, Demo demo);
+
+    JudgementManagement private judgementManagement;
+    ActivitiesManagement private activitiesManagement;
+
+    struct Demo {
+        uint256 demoId;// 从0开始
+        uint256 activityId;
+        string dataCID;
+        address submitter;
+    }
+
+    mapping(uint256 => Demo[]) public demos;// activityId => demoId => Demo
+
+    constructor(address _activitiesManagementAddress, address _judgementManagementAddress) {
+        activitiesManagement = ActivitiesManagement(_activitiesManagementAddress);
+        judgementManagement = JudgementManagement(_judgementManagementAddress);
+    }
+
+    function submitDemo(uint256 _activityId, string memory _dataCID) public 
+    {
+        require(activitiesManagement.isParticipantInActivity(_activityId, msg.sender), "Not a participant of this activity");
+        uint256 demoId = demos[_activityId].length; 
+        Demo memory newDemo = Demo({
+            demoId: demoId,
+            activityId: _activityId,
+            dataCID: _dataCID,
+            submitter: msg.sender
+        });
+        demos[_activityId].push(newDemo);
+        emit DemoSubmitted(_activityId, newDemo);
+    }
+    
+    function updateDemo(uint256 _activityId, uint256 _demoId, string memory _dataCID) public 
+    {
+        require(activitiesManagement.isParticipantInActivity(_activityId, msg.sender), "Not a participant of this activity");
+        require(_demoId >= 0 && _demoId <= demos[_activityId].length, "Invalid demo ID");
+        Demo storage demoToUpdate = demos[_activityId][_demoId];
+        require(demoToUpdate.submitter == msg.sender, "Not the submitter of this demo");
+        demoToUpdate.dataCID = _dataCID;
+        emit DemoUpdated(_activityId, demoToUpdate);
+    }
+
+}
