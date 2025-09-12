d9e9864a7431fb68bd45fa8bd664ff757abeaf44
Chris <61536264+10000-C@users.noreply.github.com>
2025-09-12 09:10:14 +0800

    feat - 添加生成judgeInvitation功能

---
+++ front/src/utils/GenerateJudgeCode.jsx
@@ -0,0 +1,20 @@
+import {ethers} from "ethers";
+import JudgementManagement from "./contracts/JudgementManagement.json"
+
+const generateJudgeCode = async (activityId,email) => {
+   const CONTRACT_ADDRESS = "0xFe09E101a92EEcE4Bc40F6d9aE8eAA2AE2F90B8a";
+
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  const signer = await provider.getSigner();
+  const contract = new ethers.Contract(CONTRACT_ADDRESS, JudgementManagement.abi, signer);
+
+  try {
+    const tx = await contract.createJudgeInvitation(activityId,email);
+    const response =  await tx.wait();
+    console.log("Judge code generated successfully:", response);
+    return response;
+  } catch (error) {
+    console.error("Error generating judge code:", error);
+  }
+};
+export default generateJudgeCode;
